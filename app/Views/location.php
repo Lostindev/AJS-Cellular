ec4f3d80331f5c6fe7a1a5ed769f683583fc3f48
Mala-Spectre <malahoko@icloud.com>
2020-11-10 12:35:12 -0500

    Location Page Tweaks | Store Page Styling |

--- app/Views/location.php
+++ app/Views/location.php
@@ -2,20 +2,23 @@
     <div class="row col-12 justify-content-center serviceDiv">
         <div class="col-12" id="locationDiv">
             <div class="d-flex text-center align-items-center blur-full col-12 justify-content-center">
-                    <h1 class="locationH1">Location</h1>
+                <h1 class="locationH1">Location</h1>
             </div>
         </div>
     </div>
 
     <div class="container-fluid locationCtnr">
         <div class="row">
+            <div class="col-lg-6 text-center mapsDiv">
+                <h2>790 N Main St, Akron, OH 44310</h2>
+                <img class="img-fluid" src="/img/map.PNG">
+
+            </div>
+
             <div class="col-lg-6 contactDiv text-center">
                 <h1 class="col-12">Contact Us</h1>
             </div>
 
-            <div class="col-lg-6 mapsDiv">
-                <img class="img-fluid" src="/img/map.PNG">
-            </div>
         </div>
     </div>
 </section>
--- app/Views/store.php
+++ app/Views/store.php
@@ -2,7 +2,21 @@
     <div class="container-fluid">
         <div class="row col-12 justify-content-center">
             <div class="col-auto home-div text-center">
-                <h1>Shop our latest inventory</h1>
+                <h2>Shop our latest inventory</h2>
+                <div class="col-12">
+                    <i class="fab fa-5x fa-apple"></i>
+                    <h3>Shop Apple</h3>
+                </div>
+
+                <div class="col-12">
+                    <i class="fab fa-5x fa-android"></i>
+                    <h3>Shop Androids</h3>
+                </div>
+
+                <div class="col-12">
+                    <i class="fas fa-5x fa-mobile"></i>
+                    <h3>Shop Accessories</h3>
+                </div>
             </div>
 
         </div>
