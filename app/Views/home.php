6e39a7daec580d91dd2b9032f2a0651fdc9b4c8f
Mala-Spectre <malahoko@icloud.com>
2020-11-11 14:58:46 -0500

    Home Page Design

--- app/Views/home.php
+++ app/Views/home.php
@@ -8,6 +8,39 @@
             </div>
         </div>
 
+        <div class="aboutContainerDiv">
+            <div class="row noStyle justify-content-between">
+                <div class="col-lg-6" id="aboutDiv">
+                    <span class="col-12 text-center">
+                        <div class="fa-ul">
+                            <br>
+                            <li><span class="fa-li"><i class="fas fa-2x fa-check"></i></span>
+                                <h3>&nbsp; Same day cell phone unlocking</h3>
+                            </li><br>
+                            <li><span class="fa-li"><i class="fas fa-2x fa-check"></i></span>
+                                <h3>&nbsp; Apple Certified Repair Center</h3>
+                            </li><br>
+                            <li><span class="fa-li"><i class="fas fa-2x fa-check"></i></span>
+                                <h3>&nbsp; Affordable phones & plans for everyone</h3>
+                            </li><br>
+                            <li><span class="fa-li"><i class="fas fa-2x fa-check"></i></span>
+                                <h3>&nbsp; We repair screens, batterys, back glass,<br> water damage & more</h3>
+                            </li><br>
+                            <li><span class="fa-li"><i class="fas fa-2x fa-check"></i></span>
+                                <h3>&nbsp; Located at 790 N Main St, Akron, Ohio</h3>
+                            </li><br>
+                        </div>
+
+                    </span>
+                </div>
+
+                <div class="col-lg-6" id="aboutPicDiv">
+                    <img class="img-fluid" id="imgPhone" src="/img/kingdom-phone.jpg">
+                </div>
+
+            </div>
+        </div>
+
 
 
     </div>
