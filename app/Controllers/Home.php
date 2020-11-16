4ae1453c19d1fab9594971f33404bc74c11ff794
Mala-Spectre <malahoko@icloud.com>
2020-11-15 21:07:42 -0500

    Home Review Different Styling as homeOther

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -19,6 +19,22 @@ class Home extends BaseController
 		echo view('users/footer', $data);
 	}
 
+	public function homeOther($page = 'homeOther')
+	{
+		if (!is_file(APPPATH . '/Views/' . $page . '.php')) {
+
+			throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
+		}
+
+		$data['title'] = 'iPhone Repair Akron Ohio | Unlock iPhones | Flashing | Jailbreaking';
+		$data['description'] = 'Akron Ohio iPhone screen repairs, back glass repairs, jailbreaking, flashing & much more! We do most android repairs also. Get cash for your phone today, stop in for an estimate.';
+		$data['page'] = $page;
+
+		echo view('users/header', $data);
+		echo view($page, $data);
+		echo view('users/footer', $data);
+	}
+
 	//--------------------------------------------------------------------
 
 }
---
+++ app/Views/homeOther.php
@@ -0,0 +1,131 @@
+<section class="d-flex align-items-center" id="home-section" name="home-section">
+    <div class="container-fluid">
+        <div class="row col-12 noStyle justify-content-center">
+            <div class="col-lg-10 col-sm-9" id="homeDiv">
+                <div class="d-flex text-center align-items-center blurFull col-12 justify-content-center">
+                    <h1 id="homeH1">#1 Source For Mobile Solutions</h1>
+                </div>
+            </div>
+        </div>
+
+        <div class="aboutContainerDiv">
+            <div class="row noStyle justify-content-between">
+                <div class="col-lg-6" id="aboutDiv">
+                    <span class="col-12 text-center">
+                        <div class="fa-ul">
+                            <br>
+                            <li><span class="fa-li"><i class="fas fa-2x fa-check"></i></span>
+                                <h3>&nbsp; Same day cell phone unlocking</h3>
+                            </li><br>
+                            <li><span class="fa-li"><i class="fas fa-2x fa-check"></i></span>
+                                <h3>&nbsp; Apple Certified Repair Center</h3>
+                            </li><br>
+                            <li><span class="fa-li"><i class="fas fa-2x fa-check"></i></span>
+                                <h3>&nbsp; Affordable phones & plans for everyone</h3>
+                            </li><br>
+                            <li><span class="fa-li"><i class="fas fa-2x fa-check"></i></span>
+                                <h3>&nbsp; We repair screens, batterys, back glass,<br> water damage & more</h3>
+                            </li><br>
+                            <li><span class="fa-li"><i class="fas fa-2x fa-check"></i></span>
+                                <h3>&nbsp; Located at 790 N Main St, Akron, Ohio</h3>
+                            </li><br>
+                        </div>
+
+                    </span>
+                </div>
+
+                <div class="col-lg-6" id="aboutPicDiv">
+                    <img class="img-fluid" id="imgPhone" src="/img/kingdom-phone.jpg">
+                </div>
+
+            </div>
+        </div>
+
+        <div class="container-fluid">
+            <div class="row col-12 aboutAjRow justify-content-center">
+                <div class="col-lg-4" id="aboutAjDiv">
+                    <div class="text-center align-items-center aboutAjStyle col-12 justify-content-center">
+                        <i class="col-12 fas fa-5x fa-tachometer-alt"></i>
+                        <p>Our reputation for fast and quality service has made us the best-rated cell phone store in the Akron and Canton area. We are proud to be the customer's choice for phone repairs and services. </p>
+                    </div>
+                </div>
+
+                <div class="col-lg-4" id="aboutAjDiv">
+                    <div class="text-center align-items-center aboutAjStyle col-12 justify-content-center">
+                        <i class="col-12 fas fa-5x fa-check-circle"></i>
+                        <p>As an Official AppleCare Repair Center you can trust us to get the job done right. </p>
+                    </div>
+                </div>
+
+                <div class="col-lg-4" id="aboutAjDiv">
+                    <div class="text-center align-items-center aboutAjStyle col-12 justify-content-center">
+                        <i class="fas fa-5x fa-map-marked-alt"></i>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis commodi ipsam sed tempore officia est quibusdam voluptates totam natus dicta quae, culpa ex accusamus officiis vero? Hic, a? Sed tenetur quis quas enim! Tempore, maxime! Labore assumenda alias iusto id.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container-fluid d-flex justify-content-center">
+            <div class="row col-10 reviewsRow text-center justify-content-center">
+                <div class="col-sm-6 col-md-4">
+                    <div class="testimonial-box blurFull">
+                        <img src="/img/reviewGoogleOne.png" class="img-responsive" alt="" width="90">
+                        <div class="ratings-icons">
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                        </div>
+                        <h4>Jainesha Ragsdale</h4>
+                        <p>"Helped me when I was in need I was paying $350 a month to sprint for an iPhone aj unlocked it a put att prepaid for $90 out the door. Got me great service for so much cheaper I love ajs phone store."</p>
+                        <i class="fas fa-star gold"></i>
+                        <i class="fas fa-star gold"></i>
+                        <i class="fas fa-star gold"></i>
+                        <i class="fas fa-star gold"></i>
+                        <i class="fas fa-star gold"></i>
+
+                    </div>
+                </div> <!-- End Col -->
+
+                <div class="col-sm-6 col-md-4">
+                    <div class="testimonial-box blurFull">
+                        <img src="https://images.pexels.com/photos/478544/pexels-photo-478544.jpeg?w=940&h=650&auto=compress&cs=tinysrgb" class="img-responsive" alt="" width="90">
+                        <div class="ratings-icons">
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                        </div>
+                        <h4>Jon Doe</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sapien augue, dictum et gravida et</p>
+
+                    </div>
+                </div> <!-- End Col -->
+
+                <div class="col-sm-6 col-md-4">
+                    <div class="testimonial-box blurFull">
+                        <img src="https://images.pexels.com/photos/478544/pexels-photo-478544.jpeg?w=940&h=650&auto=compress&cs=tinysrgb" class="img-responsive" alt="" width="90">
+                        <div class="ratings-icons">
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                        </div>
+                        <h4>Maria Jose</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sapien augue, dictum et gravida et</p>
+
+                    </div>
+                </div> <!-- End Col -->
+            </div>
+
+        </div>
+    </div>
+
+
+
+    </div>
+</section>
