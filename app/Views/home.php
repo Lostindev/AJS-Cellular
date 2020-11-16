87e6cb7deca7d5047e7c2b737d17fbca5a531812
Mala-Spectre <malahoko@icloud.com>
2020-11-15 20:43:32 -0500

    dev push

--- app/Views/home.php
+++ app/Views/home.php
@@ -46,14 +46,14 @@
                 <div class="col-lg-4" id="aboutAjDiv">
                     <div class="text-center align-items-center aboutAjStyle col-12 justify-content-center">
                         <i class="col-12 fas fa-5x fa-tachometer-alt"></i>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis commodi ipsam sed tempore officia est quibusdam voluptates totam natus dicta quae, culpa ex accusamus officiis vero? Hic, a? Sed tenetur quis quas enim! Tempore, maxime! Labore assumenda alias iusto id.</p>
+                        <p>Our reputation for fast and quality service has made us the best-rated cell phone store in the Akron and Canton area. We are proud to be the customer's choice for phone repairs and services. </p>
                     </div>
                 </div>
 
                 <div class="col-lg-4" id="aboutAjDiv">
                     <div class="text-center align-items-center aboutAjStyle col-12 justify-content-center">
                         <i class="col-12 fas fa-5x fa-check-circle"></i>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis commodi ipsam sed tempore officia est quibusdam voluptates totam natus dicta quae, culpa ex accusamus officiis vero? Hic, a? Sed tenetur quis quas enim! Tempore, maxime! Labore assumenda alias iusto id.</p>
+                        <p>As an Official AppleCare Repair Center you can trust us to get the job done right. </p>
                     </div>
                 </div>
 
@@ -70,8 +70,8 @@
             <div class="row col-10 reviewsRow justify-content-center">
                 <div class="col-lg-12" id="reviewsDiv">
                     <div class="text-center reviewsStyle row justify-content-center">
-                        <span id="reviewSpan" class="row">
-                            <div class="col-lg-3 reviewI">
+                        <span id="reviewSpan" class="row col-10 no-left-right">
+                            <div class="col-auto reviewI d-flex text-center align-items-center">
                                 <i class="fas fa-2x fa-star gold"></i>
                                 <i class="fas fa-2x fa-star gold"></i>
                                 <i class="fas fa-2x fa-star gold"></i>
@@ -79,9 +79,55 @@
                                 <i class="fas fa-2x fa-star gold"></i>
                             </div>
                             <div class="col-lg-9 reviewText">
-                                <blockquote>Always prompt service. Very knowledgeable about plans, phones etc. Great customer service.</blockquote>
+                                <i>
+                                    <p>"Always prompt service. Very knowledgeable about plans, phones etc. Great customer service."</p>
+                                </i>
                                 <div class="col-12 text-right">
                                     <p><b>- Tamara King</b></p>
+                                </div>
+                            </div>
+                        </span>
+                    </div>
+                </div>
+
+                <div class="col-lg-12" id="reviewsDiv">
+                    <div class="text-center reviewsStyle row justify-content-center">
+                        <span id="reviewSpan" class="row col-10 no-left-right">
+                            <div class="col reviewI">
+                                <i class="fas fa-2x fa-star gold"></i>
+                                <i class="fas fa-2x fa-star gold"></i>
+                                <i class="fas fa-2x fa-star gold"></i>
+                                <i class="fas fa-2x fa-star gold"></i>
+                                <i class="fas fa-2x fa-star gold"></i>
+                            </div>
+                            <div class="col-lg-9 reviewText">
+                                <i>
+                                    <p>"AJ really knows his stuff. I'm a tech nerd but went in with my better half because I didn't have time to research her issue. He quickly took care of it for next to nothing. I've never met anyone more patient and good at multi tasking. Go see him."</p>
+                                </i>
+                                <div class="col-12 text-right">
+                                    <p><b>- J Turner</b></p>
+                                </div>
+                            </div>
+                        </span>
+                    </div>
+                </div>
+
+                <div class="col-lg-12" id="reviewsDiv">
+                    <div class="text-center reviewsStyle row justify-content-center">
+                        <span id="reviewSpan" class="row col-10 no-left-right">
+                            <div class="col-lg-3 reviewI ">
+                                <i class="fas fa-2x fa-star gold"></i>
+                                <i class="fas fa-2x fa-star gold"></i>
+                                <i class="fas fa-2x fa-star gold"></i>
+                                <i class="fas fa-2x fa-star gold"></i>
+                                <i class="fas fa-2x fa-star gold"></i>
+                            </div>
+                            <div class="col-lg-9 reviewText">
+                                <i>
+                                    <p>"Highly recommended!!! His service is impeccable.. Free phones and cheap service couldn't be happier."</p>
+                                </i>
+                                <div class="col-12 text-right">
+                                    <p><b>- Erik Wagner</b></p>
                                 </div>
                             </div>
                         </span>
