56e1b4e2a3bee4c56c22460fb0300eb805083960
Mala-Spectre <malahoko@icloud.com>
2020-11-15 23:00:47 -0500

    Location Page Actual Maps Place

--- app/Views/location.php
+++ app/Views/location.php
@@ -7,15 +7,15 @@
         </div>
     </div>
 
-    <div class="container-fluid locationCtnr">
+    <div class="container locationCtnr">
         <div class="row">
-            <div class="col-lg-6 text-center mapsDiv">
+            <div class="col-auto text-center mapsDiv">
                 <h2>790 N Main St, Akron, OH 44310</h2>
-                <img class="img-fluid" src="/img/map.PNG">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3006.2731815029188!2d-81.51315538482619!3d41.10672452131017!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x883128218c302ae3%3A0x566eca745cf633d!2zQUrigJlTIENFTExVTEFS!5e0!3m2!1sen!2sus!4v1605494527298!5m2!1sen!2sus" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 
             </div>
 
-            <div class="col-lg-6 contactDiv text-center">
+            <div class="col-auto contactDiv text-center">
                 <h1 class="col-12">Contact Us</h1>
             </div>
 
